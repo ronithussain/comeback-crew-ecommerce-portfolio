a94a5929278f01fec32c678eae6b4da43363989e
ronithussain <xayaanhussain542@gmail.com>
2025-06-19 21:31:08 +0600

    my banner and category section is done

---
+++ app/home/page.jsx
@@ -0,0 +1,14 @@
+import Categories from '@/components/Categories';
+import SliderComponent from '@/components/SliderComponent';
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+          <SliderComponent/>
+          <Categories/>
+        </div>
+    );
+};
+
+export default HomePage;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,7 @@
-import Image from "next/image";
+import HomePage from "./home/page";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <HomePage />
   );
 }
